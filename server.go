ff2a9dc42879c61bd8d964f659e3e75ce747ed54
Jesse Millar <hellojessemillar@gmail.com>
2016-06-16 09:01:21 -0600

    Moving back to fasthttp after fixing the buffer issue

--- server.go
+++ server.go
@@ -9,7 +9,7 @@ import (
 	"github.com/byuoitav/wait-for-reboot-microservice/helpers"
 	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/engine/fasthttp"
 	"github.com/labstack/echo/middleware"
 )
 
@@ -47,5 +47,7 @@ func main() {
 	router.Post("/submit", submitRequest)
 
 	fmt.Printf("The Wait for Reboot microservice is listening on %s\n", port)
-	router.Run(standard.New(port))
+	server := fasthttp.New(port)
+	server.ReadBufferSize = 1024 * 10 // Needed to interface properly with WSO2
+	router.Run(server)
 }
